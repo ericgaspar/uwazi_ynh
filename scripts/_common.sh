cbc4bfc658c20b572a61053731876cb65477c661
ericgaspar <junk.eg@free.fr>
2020-09-15 16:22:05 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql apt-transport-https"
+pkg_dependencies="postgresql apt-transport-https pdftotext libjpeg-dev"
 
 nodejs_version=14
 
